33e66646cae8b92a9de3e9d2eee0728ae09d6b4b
Harish1611 <harishnaidub005@gmail.com>
2023-10-20 23:53:06 +0530

    Created MainCarouselData Component with Dummy Carosel images & rendered then in carousel

--- src/customer/components/HomeCarousel/MainCarousel.jsx
+++ src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -1,26 +1,19 @@
 import React from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
-
-const items = [
-  <div className="item" data-value="1">
-    1
-  </div>,
-  <div className="item" data-value="2">
-    2
-  </div>,
-  <div className="item" data-value="3">
-    3
-  </div>,
-  <div className="item" data-value="4">
-    4
-  </div>,
-  <div className="item" data-value="5">
-    5
-  </div>,
-];
+import { mainCarouselData } from "./MainCarouselData";
 
 const MainCarousel = () => {
+  const items = mainCarouselData.map((item) => (
+    <img
+      className="cursor-pointer"
+      style={{ height: "700px", width: "1600px", objectFit: "cover" }}
+      role="presentation"
+      src={item.image}
+      alt=""
+    />
+  ));
+
   return (
     <AliceCarousel mouseTracking items={items} controlsStrategy="alternate" />
   );
---
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,21 @@
+export const mainCarouselData = [
+  {
+    image:
+      "https://4.imimg.com/data4/HM/WC/MY-17233451/img-20160408-wa0021.jpg",
+    path: "/women/clothing/lengha_choli",
+  },
+  {
+    image: "https://4.imimg.com/data4/GC/DR/MY-6408422/img-20160408-wa0025.jpg",
+    path: "/women/clothing/lengha_choli",
+  },
+  {
+    image:
+      "https://4.imimg.com/data4/BA/OO/MY-6408422/exclusive-designer-chanderi-suit.jpg",
+    path: "/women/clothing/lengha_choli",
+  },
+  {
+    image:
+      "https://4.imimg.com/data4/YW/WA/MY-17233451/img-20160408-wa0023.jpg",
+    path: "/women/clothing/lengha_choli",
+  },
+];
