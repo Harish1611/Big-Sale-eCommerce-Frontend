a59a31776a04bf58603a7e28e80d5e1d1bfe164f
Harish1611 <harishnaidub005@gmail.com>
2023-11-14 21:52:01 +0530

    Issue Fixed: Banner Desktop Responsiveness issue resolved

--- src/customer/components/HomeBanners/HomeBanner.jsx
+++ src/customer/components/HomeBanners/HomeBanner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const HomeBanner = (props) => {
   return (
     <React.Fragment>
-      <img src={props.image} alt="banner" className=" h-[3.2rem] object-cover sm:w-full" />
+      <img src={props.image} alt="banner" className=" h-[3.2rem] object-cover sm:w-full sm:object-fit sm:h-full" />
     </React.Fragment>
   );
 };
