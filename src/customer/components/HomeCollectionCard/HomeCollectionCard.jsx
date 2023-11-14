7c619370284430b56e028a9b08a3193516fcdf51
Harish1611 <harishnaidub005@gmail.com>
2023-11-14 22:04:17 +0530

    Issue Fixed: Home Collection Slider Follow up error resolved

--- src/customer/components/HomeCollectionCard/HomeCollectionCard.jsx
+++ src/customer/components/HomeCollectionCard/HomeCollectionCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const HomeCollectionCard = ({ product }) => {
   return (
     <div className="cursor-pointer flex flex-col items-center bg-white rounded-md shadow-lg overflow-hidden w-[21rem] mx-1 sm:w-[15rem] sm:m-4 z-10">
-      <div className=" h-[24rem] sm:h-[15rem] w-full">
+      <div className=" h-[24rem] sm:h-[15rem] w-full mx-4">
         <img
           className="object-cover object-top w-full h-full z-0 transition ease-in-out delay-150 hover:translate-y-1 hover:scale-105 hover: duration-300"
           src={product?.image || product?.imageUrl}
