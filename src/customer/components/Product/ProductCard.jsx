f533faedc26380a93361600e16c963f3396f4a53
Harish1611 <harishnaidub005@gmail.com>
2023-11-15 09:41:15 +0530

    Modified: Updated responsiveness and font size of Product grid content

--- src/customer/components/Product/ProductCard.jsx
+++ src/customer/components/Product/ProductCard.jsx
@@ -1,22 +1,22 @@
 import './ProductCard.css'
 const ProductCard = ({product}) => {
   return (
-    <div  className='productCard w-[15rem] border m-3 transition-all cursor-pointer '>
-    <div className='h-[20rem]'>
+    <div  className='productCard w-[18rem] sm:w-[15rem] border m-3 transition-all cursor-pointer '>
+    <div className='h-[20rem] sm:h-[17rem]'>
         <img className='h-full w-full object-cover object-left-top' src={product.imageUrl} alt="" />
     </div>
     <div className='textPart bg-white p-3 '>
         <div>
         <p  className='font-bold opacity-60'>{product.brand}</p>
-            <p className=''>{product.title}</p>
+            <p className='sm:text-sm '>{product.title}</p>
         
-        <p className='font-semibold opacity-50'>{product.title}</p>
+        <p className='font-semibold sm:text-sm  opacity-50'>{product.color}</p>
         </div>
         
         <div className='flex space-x-2 items-center'>
-            <p className='font-semibold'>₹{product.discountedPrice}</p>
-            <p className='opacity-50 line-through'>₹{product.price}</p>
-            <p className='text-green-600 font-semibold'>{product.discountPersent}% off</p>
+            <p className='font-semibold sm:text-sm '>₹{product.discountedPrice}</p>
+            <p className='opacity-50 line-through sm:text-sm '>₹{product.price}</p>
+            <p className='text-green-600 font-semibold sm:text-sm '>{product.discountPersent}% off</p>
         </div>
         
     </div>
