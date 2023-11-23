aa76994903f2ca4a963ca9b1bcc34a5ef7abcd53
Harish1611 <harishnaidub005@gmail.com>
2023-11-23 21:34:48 +0530

    New: Created Product Actions for find product & find product Id using axios calls

---
+++ src/state/Product/Action.js
@@ -0,0 +1,71 @@
+import axios from "axios";
+
+import {
+  FIND_PRODUCTS_BY_CATEGORY_REQUEST,
+  FIND_PRODUCTS_BY_CATEGORY_SUCCESS,
+  FIND_PRODUCTS_BY_CATEGORY_FAILURE,
+  FIND_PRODUCT_BY_ID_REQUEST,
+  FIND_PRODUCT_BY_ID_SUCCESS,
+  FIND_PRODUCT_BY_ID_FAILURE,
+
+} from "./ActionType";
+import api, { API_BASE_URL } from "../../config/apiConfig";
+
+export const findProducts = (reqData) => async (dispatch) => {
+  const {
+    colors,
+    sizes,
+    minPrice,
+    maxPrice,
+    minDiscount,
+    category,
+    stock,
+    sort,
+    pageNumber,
+    pageSize,
+  } = reqData;
+
+  try {
+    dispatch({ type: FIND_PRODUCTS_BY_CATEGORY_REQUEST });
+
+    const { data } = await api.get(
+      `/api/products?color=${colors}&size=${sizes}&minPrice=${minPrice}&maxPrice=${maxPrice}&minDiscount=${minDiscount}&category=${category}&stock=${stock}&sort=${sort}&pageNumber=${pageNumber}&pageSize=${pageSize}`
+    );
+
+    console.log("get product by category - ", data);
+    dispatch({
+      type: FIND_PRODUCTS_BY_CATEGORY_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: FIND_PRODUCTS_BY_CATEGORY_FAILURE,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const findProductById = (reqData) => async (dispatch) => {
+  try {
+    dispatch({ type: FIND_PRODUCT_BY_ID_REQUEST });
+
+    const { data } = await api.get(`/api/products/id/${reqData.productId}`);
+
+    console.log("products by  id : ", data);
+    dispatch({
+      type: FIND_PRODUCT_BY_ID_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: FIND_PRODUCT_BY_ID_FAILURE,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
