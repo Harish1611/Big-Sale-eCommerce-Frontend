17e45a143f85f187a20557e91902aeac85074cb2
Harish1611 <harishnaidub005@gmail.com>
2023-11-23 18:49:25 +0530

    Modified: configured axios api with baseURL and headers in apiConfig file

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,3 +1,15 @@
+import axios from "axios";
+
 const LOCALHOST='http://localhost:5454'
 
+const jwt = localStorage.getItem('jwt')
+
 export const API_BASE_URL = LOCALHOST;
+
+export const api = axios.create({
+    baseURL:LOCALHOST,
+    headers:{
+        "Authorization":`Bearer ${jwt}`,
+        "Content-Type":"application/json"
+    }
+})
