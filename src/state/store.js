ddfd261b14f975de7d4531bb1765e4e3b5111b11
Harish1611 <harishnaidub005@gmail.com>
2023-11-23 22:00:19 +0530

    New: Created Order Action, ActionType & Reducter using axios & redux state

--- src/state/store.js
+++ src/state/store.js
@@ -2,12 +2,16 @@ import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
 import authReducer from "./Auth/Reducer";
 import customerProductReducer from "./Product/Reducer";
+import cartReducer from "./Cart/Reducer";
+import { orderReducer } from "./Order/Reducer";
 
 
 const rootReducers = combineReducers({
 
     auth:authReducer,
-    product:customerProductReducer
+    product:customerProductReducer,
+    cart:cartReducer,
+    order:orderReducer
 
 
 })
