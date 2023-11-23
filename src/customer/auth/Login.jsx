297c1e8bcf4b47b84266adaa349b654e0a1faa03
Harish1611 <harishnaidub005@gmail.com>
2023-11-23 14:44:32 +0530

    New: Login component created with submission handler

--- src/customer/auth/Login.jsx
+++ src/customer/auth/Login.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import { Grid, TextField, Button} from "@mui/material";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
 
-export default Login
+    const userData = {
+      email: data.get("email"),
+      password: data.get("password"),
+    };
+    console.log("login user", userData);
+  };
+  return (
+    <div>
+      {" "}
+      <form className="w-full" onSubmit={handleSubmit}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="email"
+              name="email"
+              label="Email"
+              fullWidth
+              autoComplete="given-name"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="password"
+              name="password"
+              label="Password"
+              fullWidth
+              autoComplete="given-name"
+              type="password"
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button
+              className="bg-[#9155FD] w-full"
+              type="submit"
+              variant="contained"
+              size="large"
+              sx={{ padding: ".8rem 0" }}
+            >
+              Login
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
