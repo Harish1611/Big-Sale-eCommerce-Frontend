3d30973c1f93dc3e39ef0f32fb36726b627b5b14
Harish1611 <harishnaidub005@gmail.com>
2023-11-23 14:19:34 +0530

    New: Created a AuthModal component using MUI component for Login & Register

---
+++ src/customer/auth/AuthModal.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Box, Modal, Typography } from "@mui/material";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 500,
+  bgcolor: "background.paper",
+  outline: "none",
+  boxShadow: 24,
+  p: 4,
+};
+
+const AuthModal = (handleClose, open) => {
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default AuthModal;
