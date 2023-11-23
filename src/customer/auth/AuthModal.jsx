f8a50d0c9fae1944097ab3e02edb5af14b7a841d
Harish1611 <harishnaidub005@gmail.com>
2023-11-23 18:15:44 +0530

    Modified: Added authModel close handler function and setup user state using redux

--- src/customer/auth/AuthModal.jsx
+++ src/customer/auth/AuthModal.jsx
@@ -1,8 +1,10 @@
-import React from "react";
-import { Box, Modal, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
 import RegisterForm from "./RegisterForm";
-import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Login from "./Login";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const style = {
   position: "absolute",
@@ -11,28 +13,39 @@ const style = {
   transform: "translate(-50%, -50%)",
   width: 500,
   bgcolor: "background.paper",
-  outline: "none",
   boxShadow: 24,
   p: 4,
 };
 
-const AuthModal = (handleClose, open) => {
-    const location = useLocation();
+export default function AuthModal({ handleClose, open }) {
+  const location = useLocation();
+  const { auth } = useSelector((store) => store);
+  const navigate=useNavigate()
+  useEffect(() => {
+    if (auth.user){
+       handleClose();
+       
+      }
+  }, [auth.user]);
   return (
-    <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-            { location.pathname === '/login' ? <Login />:<RegisterForm />
-}
-        </Box>
-      </Modal>
-    </div>
+    <>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+      size="large"
+    >
+      <Box className="rounded-md" sx={style}>
+        {location.pathname === "/login" ? (
+          <Login />
+        ) : (
+          <RegisterForm />
+        )}
+      </Box>
+    </Modal>
+    
+    </>
+    
   );
-};
-
-export default AuthModal;
+}
