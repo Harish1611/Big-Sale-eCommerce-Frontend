cdcfe96a297651f806b52da7be1262ebfd8ef8f7
Harish1611 <harishnaidub005@gmail.com>
2023-11-24 20:34:41 +0530

    New: Integrated Cyclic Backend URL - Connection Working Properly

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+const CYCLIC_BACKEND = 'https://plum-chameleon-suit.cyclic.app/'
+
 const LOCALHOST='http://localhost:5454'
 
 const jwt = localStorage.getItem('jwt')
 
-export const API_BASE_URL = LOCALHOST;
+export const API_BASE_URL = CYCLIC_BACKEND;
 
 export const api = axios.create({
     baseURL:API_BASE_URL,
