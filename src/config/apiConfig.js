ed54e9165d2601c5a046e05d7efc1add2178e7da
Harish1611 <harishnaidub005@gmail.com>
2023-11-27 14:32:05 +0530

    Modified: New Backend Deployed and integrated with frontend

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const CYCLIC_BACKEND = 'https://plum-chameleon-suit.cyclic.app/'
+const CYCLIC_BACKEND = 'https://aggressive-windbreaker-lamb.cyclic.app/'
 
 const LOCALHOST='http://localhost:5454'
 
 const jwt = localStorage.getItem('jwt')
 
-export const API_BASE_URL = CYCLIC_BACKEND ;
+export const API_BASE_URL = CYCLIC_BACKEND;
 
 export const api = axios.create({
     baseURL:API_BASE_URL,
